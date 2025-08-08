7ff52cb0eb4c88e0871b8c43790b025b20708ad5
VivekRupapara146 <142913780+VivekRupapara146@users.noreply.github.com>
2025-08-09 00:04:52 +0530

    Add files via upload

---
+++ 01_Notes.py
@@ -0,0 +1,58 @@
+# list1 = [ "apple", "banana"]
+# list_2 = [ 1 , 2 , 3]
+
+# print(type(list1))
+#  print(type(list1[1]))
+# print(type(list_2))
+# print(type(list_2[1]))
+
+# extended_list = [4, 5, 6, 7, 8, 9]
+# list_2.extend(extended_list)
+# print(list_2)
+
+# .append adds element at end of the list
+# .index()
+# ORDERED MUTABLE COLLECTION
+#  .remove(i) removes item whose value(value not in list error) is i
+# in case of multiple same values first value will be removed
+# .pop() removes element at index(index out of range error) if specified or last element and returns it as well
+# mutable
+
+# TUPLE my_tuple  = ()
+# .index()
+# count(element)
+# in case of multiple same values first value will be given
+# NON MUTABLE UNORDERED collection
+# my_tuple = (1, 2, 3, [4, 5, 7], 7 , 8)
+# my_tuple[3][2] = 6
+# print(my_tuple)
+
+# set = {}
+# UNORDERED collection of unique items
+# set1 = {"apple", "banana", "cherry", "mango"}
+# print(set1)
+# different output every time
+# no duplicate values stored if given
+# can't change the givven values but add new ones after creating the set
+# Accessing items - 1)loops 2)'in' statement 3).add() 4).remove()
+# set.clear() to clear the set
+
+# dictionary = dict{}
+# my_dict = {"key" : "value"}
+# info = {
+#     "name" : "Vivek Rupapara"
+#     "grade" : "A"
+#     "marks" : 98
+# }
+# add/update items my_dict["new_key"] = "new_value"
+# access with help of key give key returns valus
+# methods 1) .values()  2).keys() 3).items()
+# 4).pop(key) remove key and return value
+
+# result = 5 & 3
+# print(result)
+# r2 = 0b0110 & 0b0011
+# print(bin(r2)) # with 0b
+# print(bin(r2)[2:]) # without 0b
+# print(format(r2, '04b')) # with padded 4 bits
+
